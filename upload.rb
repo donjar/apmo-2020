a3ca9375abe38b606cc6324140d4e1a2c2a45b7c
Herbert Ilhan Tanujaya <mail@herbert.id>
2019-07-20 15:39:00 +0800

    Production readY

--- upload.rb
+++ upload.rb
@@ -1,10 +1,12 @@
 require 'aws-sdk-s3'
 
 def upload_problem(file, type)
+  throw 'g' if file[:tempfile].size > 10 * (2**30)
+
   s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
 
   bucket = s3.bucket(ENV['AWS_BUCKET'])
-  wordlist = f.split("\n")
+  wordlist = File.read('wordlist').split("\n")
   10.times do
     object_name = "#{type}-#{wordlist.sample}-#{file[:filename]}"
     next if bucket.objects.any? { |item| item.key == object_name }
